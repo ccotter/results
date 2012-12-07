36e8974cb91b5f3bd7affc4e28a9b90c45508118
Chris Cotter <ccotter@utexas.edu>
2012-12-07 14:40:19 -0600

    Update LU tool

--- tools/lu.py
+++ tools/lu.py
@@ -2,17 +2,31 @@
 
 # To format pthread LU evals into CSV format.
 
+def getstat(ls):
+    assert(len(ls) == 10)
+    avg = 0
+    for i in range(len(ls)):
+        avg += float(ls[i])
+    avg /= len(ls)
+    dev = 0
+    for i in range(len(ls)):
+        dev += (float(ls[i]) - avg) ** 2
+    dev /= len(ls)
+    dev **= .5
+    return avg, dev
+
+
 def readfile(n):
     f = "../LU/pthread%d" % n
     fl = open(f)
     lines = fl.readlines()
-    print("N=%d" % n)
-    for i in range(int(len(lines) / 2)):
-        sz = lines[2*i].split(" ")[2]
-        t = float(lines[2*i+1].split(":")[1])
-        print("%s,%f" % (sz,t))
-    print()
+    for i in range(int(len(lines) / 11)):
+        ls = lines[11*i].split(" ")
+        avg, dev = getstat(lines[11*i+1:11*(i+1)])
+        print(ls[2] + "," + repr(avg) + "," + repr(dev))
 
 for i in range(4):
-        readfile(2 ** i)
+    print("N=" + repr(2**i))
+    readfile(2 ** i)
+    print()
 
