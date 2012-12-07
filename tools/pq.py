3f7ef85f94df0da63ac7e23eb70767a829c7aebb
Chris Cotter <ccotter@utexas.edu>
2012-12-07 15:58:08 -0600

    pqsort tool

--- tools/pq.py
+++ tools/pq.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python
 
-# To format pqsort evals into CSV format (pthreads and deterministic).
+# To format pqsort evals into CSV format.
 
-def readfile(n, s):
+def getstat(ls):
+    assert(len(ls) == 10)
+    avg = 0
+    for i in range(len(ls)):
+        avg += float(ls[i])
+    avg /= len(ls)
+    dev = 0
+    for i in range(len(ls)):
+        dev += (float(ls[i]) - avg) ** 2
+    dev /= len(ls) - 1
+    dev **= .5
+    return avg, dev
+
+
+def readfile(s, n):
     f = "../pqsort/%s%d" % (s, n)
     fl = open(f)
     lines = fl.readlines()
-    print("N=%d" % n)
-    for i in range(1, int(len(lines))):
-        l = lines[i].split(" ")
-        sz = int(l[1].split("\t")[0])
-        t = float(l[3])
-        print("%d,%f" % (sz, t))
-    print()
+    for i in range(int(len(lines) / 11)):
+        ls = lines[11*i].split(" ")
+        avg, dev = getstat(lines[11*i+1:11*(i+1)])
+        print(ls[1][:-2] + "," + repr(avg) + "," + repr(dev))
 
 for i in range(4):
-        readfile(2 ** i, "pthread")
+    print("N=" + repr(2**i))
+    readfile("pthread", 2 ** i)
+    print()
 for i in range(4):
-        readfile(2 ** i, "det")
+    print("N=" + repr(2**i))
+    readfile("det", 2 ** i)
+    print()
 
